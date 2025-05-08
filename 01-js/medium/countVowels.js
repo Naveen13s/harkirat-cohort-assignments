94944616115020b88705c363f0e00bb2dce9e4ed
Naveen kumar <55360964+Naveen13s@users.noreply.github.com>
2025-05-08 20:45:51 +0530

    Solution Added

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,34 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let vowelsCount = 0;
+  const charactersArray = str.toLowerCase().split("");
+  let vowels = ["a", "e", "i", "o", "u"];
+  charactersArray.forEach((character) => {
+    vowels.forEach((vowel) => {
+      if (vowel === character) {
+        vowelsCount += 1;
+      }
+    });
+  });
+  console.log(vowelsCount)
+  return vowelsCount;
 }
 
 module.exports = countVowels;
+
+
+/* PASS  01-js/tests/countVowels.test.js
+  countVowels
+    √ returns the correct count for strings with vowels (28 ms)                                                                                                       
+    √ returns 0 for strings without vowels (10 ms)
+    √ returns 0 for an empty string (3 ms)                                                                                                                            
+    √ handles case-insensitivity correctly (11 ms)                                                                                                                    
+    √ returns the correct count for strings with spaces (12 ms)                                                                                                       
+    √ returns the correct count for strings with punctuation marks (11 ms)                                                                                            
+                                                                                                                                                                      
+Test Suites: 1 passed, 1 total
+Tests:       6 passed, 6 total
+Snapshots:   0 total
+Time:        0.96 s
+Ran all test suites matching /.\\tests\\countVowels.test.js/i.*/
